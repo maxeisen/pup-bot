3d62981ace4c01ae43941e6fd5808eb267532b57
Max Eisen <17mje@queensu.ca>
2021-12-23 20:15:09 -0500

    Initial commit on new branch

---
+++ constants.py
@@ -0,0 +1,31 @@
+# Constants
+REFERENCE_AMOUNT = 1000000 #PUP tokens
+FIATS = 'CAD,USD'
+PREFERRED_FIAT = 'CAD'
+PUBLIC_FIAT = 'USD'
+CONTRACT_ADDRESS = '0x2696Fc1896F2D5F3DEAA2D91338B1D2E5f4E1D44'
+HYPE_PHRASES = [
+  "Hold on tight - we're going to the moon! 🌕",
+  "The rocket ship is taking off! 🚀",
+  "The next SHIB is here! 🐶"
+  "We're taking off! Don't get left behind! 📈",
+  "Pup pup and away! 🚀",
+  "Strap in puppies - the rocket ship has taken off! 🚀",
+  "You should've bought the dip! 😏"
+]
+STABLE_PHRASES = [
+  "Get in before the moon! 🌕",
+  "Hop on the rocket ship! 🚀",
+  "You missed SHIB... Don't miss PUP! 🐶",
+  "Don't miss out on the hottest new meme coin! 🐶",
+  "Get in early! ⏱",
+  "Become a pupillionaire! 🐶🤑"
+]
+DIP_PHRASES = [
+  "Get it while it's cheap! 🤑",
+  "Buy the dip! 📉📈🚀",
+  "HODL puppies! 🐶",
+  "Time to buy up some more! 😎",
+  "This is the time to buy! 🤑",
+  "Don't miss out on this fire sale! 🔥🏷"
+]
---
+++ pupdate.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from twilio.rest import Client
+from retry import retry
+import tweepy
+import requests
+import random
+import constants
+import secrets
+
+# Authenticate and initialize Tweepy instance 
+def initializeTweepy():
+  auth=tweepy.OAuthHandler(secrets.API_KEY, secrets.API_KEY_SECRET)
+  auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_TOKEN_SECRET)
+  t=tweepy.API(auth)
+  return t
+
+# Authenticate and initialize Twilio instance
+def initializeTwilio():
+  client=Client(secrets.TWILIO_SID, secrets.TWILIO_SECRET)
+  return client
+
+# Initialize webdriver
+def initializeWebdriver():
+  options=Options()
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  s=Service(ChromeDriverManager().install())
+  driver=webdriver.Chrome(service=s, options=options)
+  wait=WebDriverWait(driver, 10)
+  return (driver, wait)
+
+# Pull required conversion values
+@retry()
+def getConversionValues(fiats=constants.FIATS, preferredFIAT=constants.PREFERRED_FIAT, publicFIAT = constants.PUBLIC_FIAT, contract=constants.CONTRACT_ADDRESS):
+  driver, wait = initializeWebdriver()
+  reqURL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms='+fiats
+  r = requests.get(reqURL)
+  ethToPrefFIAT = r.json()[preferredFIAT]
+  ethToPubFIAT = r.json()[publicFIAT]
+  print("1 ETH is $"+str(ethToPrefFIAT)+" "+preferredFIAT+" or $"+str(ethToPubFIAT)+" "+publicFIAT)
+  uniswapURL = 'https://app.uniswap.org/#/add/ETH/'+contract+'/10000'
+  driver.get(uniswapURL)
+  pupPerEthElement = wait.until(EC.visibility_of_element_located((By.XPATH, "//html/body/div[1]/div/div[2]/div[4]/main/div[2]/div/div[4]/div[1]/div[2]/div[2]/span")))
+  ethToPUP = float(pupPerEthElement.get_attribute("innerHTML").splitlines()[0])
+  driver.quit()
+  print("1 ETH is "+str(ethToPUP)+" PUP")
+  return (ethToPubFIAT, ethToPrefFIAT, ethToPUP)
+
+# Get previous value of 1000000 PUP from last Tweet
+def getLastReferenceValue():
+  t = initializeTweepy()
+  lastTweet = t.user_timeline(user_id=secrets.BOT_ID, count=1)[0]
+  lastReferencePrice = float((lastTweet.text.split('$')[2]).split(' ')[0])
+  return lastReferencePrice
+
+# Generate full price report statement
+def generatePriceReport(delta, currentReferenceValue):
+  plusMinus = 'Up ' if (delta > 0) else 'Down '
+  deltaStatement = plusMinus+str('{0:.2f}'.format(delta))+"% since last update. "
+  priceStatement = "1 million $PUP is currently worth $"+currentReferenceValue+" "+constants.PUBLIC_FIAT+". "
+  if (delta < -2):
+    closingStatement = random.choice(constants.DIP_PHRASES)
+  elif (delta > 10):
+    closingStatement = random.choice(constants.HYPE_PHRASES)
+  else:
+    closingStatement = random.choice(constants.STABLE_PHRASES)
+  priceReport = priceStatement+deltaStatement+closingStatement+" #PuppyCoin"
+  return(priceReport)
+
+# Post tweet
+def postTweet(tweet):
+  t = initializeTweepy()
+  t.update_status(tweet)
+  print("Tweeted: "+tweet)
+
+# Send text message
+def sendText(message):
+  client = initializeTwilio()
+  for num in secrets.RECIPIENT_NUMBERS:
+    client.messages.create(body=message,from_=secrets.TWILIO_PHONE,to=num)
+    print("Sent to "+num+": " + message)
+
+# Send direct message on Twitter
+def sendDirectMessage(message, recipients=secrets.RECIPIENT_IDS):
+  t = initializeTweepy()
+  for uid in recipients:
+    t.send_direct_message(uid, message)
+    print("Sent to "+uid+": " + message)
+
+def main():
+  ethToPubFIAT, ethToPrefFIAT, ethToPUP = getConversionValues()
+  lastReferenceValue = getLastReferenceValue()
+  currentReferenceValue = str('{0:.2f}'.format(constants.REFERENCE_AMOUNT/(ethToPUP/ethToPubFIAT)))
+  delta = ((float(currentReferenceValue)-lastReferenceValue)/lastReferenceValue)*100
+  priceReport = generatePriceReport(delta, currentReferenceValue)
+
+  positionValue = str('{0:.2f}'.format(secrets.PERSONAL_HOLDINGS/(ethToPUP/ethToPrefFIAT)))
+  positionReport = "Your $PUP is worth $"+positionValue+" "+constants.PREFERRED_FIAT+" before Gas fees"
+  print(positionReport)
+
+  postTweet(priceReport)
+  sendDirectMessage(positionReport, recipients=[secrets.BOT_ID])
+  if (delta >= secrets.ALERT_DELTA_THRESHOLD):
+    personalDeltaStatement = ' - up '+str('{0:.2f}'.format(delta))+"% in the last hour!"
+    sendText(positionReport+personalDeltaStatement)
+    sendDirectMessage(positionReport+personalDeltaStatement)
+
+if __name__ == "__main__":
+  main()
