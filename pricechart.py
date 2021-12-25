699b76c6a96d0f079647444f7840caf96657f772
Max Eisen <17mje@queensu.ca>
2021-12-25 01:58:09 -0500

    Fix current time selection

--- pricechart.py
+++ pricechart.py
@@ -3,21 +3,21 @@ import numpy as np
 from PIL import Image
 from datetime import datetime
 
-def generatePriceChart(hour, price):
+def generatePriceChart(hours, prices):
   plt.style.use('seaborn-dark')
-  plt.plot(hour, price, c='silver')
+  plt.plot(hours, prices, c='silver')
 
   for i in range(10):
     if i == 9:
-      plt.plot(hour[i], price[i], marker='*', ms=15, c='gold' )
+      plt.plot(hours[i], prices[i], marker='*', ms=15, c='gold' )
     else:
-      pointStyle = 'g^' if (price[i+1] >= price[i]) else 'rv'
-      plt.plot(hour[i], price[i], pointStyle, markersize=8)
+      pointStyle = 'g^' if (prices[i+1] >= prices[i]) else 'rv'
+      plt.plot(hours[i], prices[i], pointStyle, markersize=8)
 
   plt.title("Puppy Coin Pupdates")
   plt.xlabel("Time (EST)")
   plt.ylabel("Price per 1 million $PUP (USD)")
-  plt.xticks(hour)
+  plt.xticks(hours)
   plt.grid()
   currentTimeString = datetime.now().strftime("%m%d%Y_%H%M%S")
   filename = 'pricechart_'+currentTimeString
